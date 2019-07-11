3b53d44f6ce64548cb47406c948962827133ff88
Humra <matija.huremovic@gmail.com>
2019-07-11 16:50:34 +0200

    Attributes and attributes UI panel

--- InventorySystem/Assets/Scripts/Managers/UIManager.cs
+++ InventorySystem/Assets/Scripts/Managers/UIManager.cs
@@ -5,8 +5,17 @@ public class UIManager : MonoBehaviour
 {
     private GameObject _inventoryPanel;
     private GameObject _equipmentPanel;
+    private GameObject _attributesPanel;
     private GameObject _inventoryButton;
     private GameObject _equipmentButton;
+    private GameObject _attributesButton;
+
+    private Text _str;
+    private Text _dex;
+    private Text _con;
+    private Text _int;
+    private Text _wis;
+    private Text _cha;
 
     private void Start()
     {
@@ -14,9 +23,21 @@ public class UIManager : MonoBehaviour
         _inventoryButton = GameObject.FindGameObjectWithTag("InventoryBtn");
         _equipmentPanel = GameObject.FindGameObjectWithTag("EquipmentPanel");
         _equipmentButton = GameObject.FindGameObjectWithTag("EquipmentBtn");
+        _attributesPanel = GameObject.FindGameObjectWithTag("AttributesPanel");
+        _attributesButton = GameObject.FindGameObjectWithTag("AttributesBtn");
+
+        _str = GameObject.FindGameObjectWithTag("StrValue").GetComponent<Text>();
+        _dex = GameObject.FindGameObjectWithTag("DexValue").GetComponent<Text>();
+        _con = GameObject.FindGameObjectWithTag("ConValue").GetComponent<Text>();
+        _int = GameObject.FindGameObjectWithTag("IntValue").GetComponent<Text>();
+        _wis = GameObject.FindGameObjectWithTag("WisValue").GetComponent<Text>();
+        _cha = GameObject.FindGameObjectWithTag("ChaValue").GetComponent<Text>();
+
+        _updateAttributeValues();
 
         _inventoryPanel.SetActive(false);
         _equipmentPanel.SetActive(false);
+        _attributesPanel.SetActive(false);
     }
 
     private void Update()
@@ -29,6 +50,20 @@ public class UIManager : MonoBehaviour
         {
             ToggleEquipment();
         }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleAttributes();
+        }
+    }
+
+    private void _updateAttributeValues()
+    {
+        _str.text = PlayerAttributes.Strength.ToString();
+        _dex.text = PlayerAttributes.Dexterity.ToString();
+        _con.text = PlayerAttributes.Constitution.ToString();
+        _int.text = PlayerAttributes.Intelligence.ToString();
+        _wis.text = PlayerAttributes.Wisdom.ToString();
+        _cha.text = PlayerAttributes.Charisma.ToString();
     }
 
     public void ToggleInventory()
@@ -41,5 +76,11 @@ public class UIManager : MonoBehaviour
     {
         _equipmentPanel.SetActive(!_equipmentPanel.activeSelf);
         _equipmentButton.SetActive(!_equipmentButton.activeSelf);
+    }
+
+    public void ToggleAttributes()
+    {
+        _attributesPanel.SetActive(!_attributesPanel.activeSelf);
+        _attributesButton.SetActive(!_attributesButton.activeSelf);
     }
 }
