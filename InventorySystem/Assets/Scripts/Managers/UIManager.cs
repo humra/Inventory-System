fd89e6cf9ec706aba7e6ea98a91482d50c6c7a07
Humra <matija.huremovic@gmail.com>
2019-07-11 15:09:33 +0200

    Opening and closing of inventory and equipment

---
+++ InventorySystem/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private GameObject _inventoryPanel;
+    private GameObject _equipmentPanel;
+    private GameObject _inventoryButton;
+    private GameObject _equipmentButton;
+
+    private void Start()
+    {
+        _inventoryPanel = GameObject.FindGameObjectWithTag("InventoryPanel");
+        _inventoryButton = GameObject.FindGameObjectWithTag("InventoryBtn");
+        _equipmentPanel = GameObject.FindGameObjectWithTag("EquipmentPanel");
+        _equipmentButton = GameObject.FindGameObjectWithTag("EquipmentBtn");
+
+        _inventoryPanel.SetActive(false);
+        _equipmentPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInventory();
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleEquipment();
+        }
+    }
+
+    public void ToggleInventory()
+    {
+        _inventoryPanel.SetActive(!_inventoryPanel.activeSelf);
+        _inventoryButton.SetActive(!_inventoryButton.activeSelf);
+    }
+
+    public void ToggleEquipment()
+    {
+        _equipmentPanel.SetActive(!_equipmentPanel.activeSelf);
+        _equipmentButton.SetActive(!_equipmentButton.activeSelf);
+    }
+}
