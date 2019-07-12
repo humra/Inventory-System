5c7ac9143d0e1fac351a6b910042b592d749766a
Humra <matija.huremovic@gmail.com>
2019-07-12 22:21:35 +0200

    Created more items and created the base for stacking items in the inventory

---
+++ InventorySystem/Assets/Scripts/Items/NonEquippableItem.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New NonEquippable Item", menuName = "Inventory/Non-Equippable")]
+public class NonEquippableItem : Item
+{
+    public EnumStackable Stackable;
+    public int MaximumStack;
+}
