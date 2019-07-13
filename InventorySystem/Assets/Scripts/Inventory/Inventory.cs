b52a9d9d12797dc5693c10bd4eb631edb464c4b7
Humra <matija.huremovic@gmail.com>
2019-07-13 15:33:34 +0200

    Items can now be swapped in inventory, but no longer follow the cursor

--- InventorySystem/Assets/Scripts/Inventory/Inventory.cs
+++ InventorySystem/Assets/Scripts/Inventory/Inventory.cs
@@ -24,6 +24,9 @@ public class Inventory : MonoBehaviour
     private int _inventoryCapacity;
     private InventorySlot[] _inventorySlots;
     private List<Item> _items = new List<Item>();
+    private Item _temporaryItem;
+    private int _temporaryStackCount;
+    private int _originIndex;
 
     private void Start()
     {
@@ -35,7 +38,7 @@ public class Inventory : MonoBehaviour
     {
         for(int i = 0; i < _items.Count; i++)
         {
-            _inventorySlots[i].AddItem(_items[i]);
+            _inventorySlots[i].SetItem(_items[i]);
             _inventorySlots[i].UpdateInventorySlot();
         }
     }
@@ -78,5 +81,42 @@ public class Inventory : MonoBehaviour
     {
         _items.Remove(removedItem);
         _updateInventorySlots();
+    }
+
+    public void SetTemporaryItemData(InventorySlot inventorySlot)
+    {
+        _temporaryItem = inventorySlot.GetItem();
+        _temporaryStackCount = inventorySlot.GetStackCount();
+        _originIndex = GetIndexOfInventorySlot(inventorySlot);
+    }
+
+    public void SwapItemToDestination(InventorySlot inventorySlot)
+    {
+        inventorySlot.SetItem(_temporaryItem);
+        inventorySlot.SetStackCount(_temporaryStackCount);
+        _inventorySlots[_originIndex].ClearSlot();
+    }
+
+    public bool TemporaryItemExists()
+    {
+        return _temporaryItem != null;
+    }
+
+    public void ClearTemporaryItem()
+    {
+        _temporaryItem = null;
+    }
+
+    public int GetIndexOfInventorySlot(InventorySlot inventorySlot)
+    {
+        for(int i = 0; i < _inventorySlots.Length; i++)
+        {
+            if(_inventorySlots[i] == inventorySlot)
+            {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
--- InventorySystem/Assets/Scripts/Inventory/InventorySlot.cs
+++ InventorySystem/Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,25 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : EventTrigger
 {
     private Item _item;
     private Image _icon;
     private Text _stackText;
     private int _stackCount = 0;
-    private ItemButton _itemButton;
 
     private void Start()
     {
-        _icon = transform.Find("ItemButton/ContentParent/Image").GetComponent<Image>();
-        _stackText = transform.Find("ItemButton/ContentParent/StackCount").GetComponent<Text>();
+        _icon = transform.Find("ContentParent/Image").GetComponent<Image>();
+        _stackText = transform.Find("ContentParent/StackCount").GetComponent<Text>();
         _stackText.text = "";
         _icon.enabled = false;
-        _itemButton = GetComponentInChildren<ItemButton>();
     }
 
-    public void AddItem(Item newItem)
+    public void SetItem(Item newItem)
     {
+        if(newItem == null)
+        {
+            ClearSlot();
+        }
+
         _item = newItem;
         _icon.sprite = newItem.Icon;
         _icon.enabled = true;
@@ -32,8 +36,6 @@ public class InventorySlot : MonoBehaviour
         {
             _stackText.text = _stackCount.ToString();
         }
-
-        _itemButton.HasItem = true;
     }
 
     public void ClearSlot()
@@ -43,7 +45,6 @@ public class InventorySlot : MonoBehaviour
         _icon.enabled = false;
         _stackCount = 0;
         _stackText.text = "";
-        _itemButton.HasItem = false;
     }
 
     public bool AddOneItem()
@@ -88,12 +89,49 @@ public class InventorySlot : MonoBehaviour
         return _stackCount;
     }
 
+    public void SetStackCount(int stackCount)
+    {
+        _stackCount = stackCount;
+    }
+
     public void UpdateInventorySlot()
     {
         _icon.sprite = _item.Icon;
         if (_stackCount >= 2)
         {
             _stackText.text = _stackCount.ToString();
+        }
+    }
+
+    public Item GetItem()
+    {
+        return _item;
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if(_item != null)
+        {
+            switch (eventData.button)
+            {
+                case PointerEventData.InputButton.Left:
+                    Debug.Log("Left click");
+                    Inventory.Instance.SetTemporaryItemData(this);
+                    break;
+                case PointerEventData.InputButton.Right:
+                    Debug.Log("Right click");
+                    break;
+                case PointerEventData.InputButton.Middle:
+                    Debug.Log("Middle click");
+                    break;
+            }
+        }
+        else
+        {
+            if(eventData.button == PointerEventData.InputButton.Left && Inventory.Instance.TemporaryItemExists())
+            {
+                Inventory.Instance.SwapItemToDestination(this);
+            }
         }
     }
 }
