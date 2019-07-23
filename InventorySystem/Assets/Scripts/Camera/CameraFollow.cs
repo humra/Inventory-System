2196c8cc8ef932f8d6ea798891325cfbd1f6127c
Humra <matija.huremovic@gmail.com>
2019-07-23 22:40:09 +0200

    Camera now follows the player with a delay

--- InventorySystem/Assets/Scripts/Camera/CameraFollow.cs
+++ InventorySystem/Assets/Scripts/Camera/CameraFollow.cs
@@ -6,6 +6,8 @@ public class CameraFollow : MonoBehaviour
 
     [SerializeField]
     private Vector3 _offset = new Vector3(0, 0, -10);
+    [SerializeField]
+    private float _followSpeed = 3f;
 
     private void Start()
     {
@@ -14,6 +16,7 @@ public class CameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = _player.position + _offset;
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, _player.transform.position.x, Time.deltaTime * _followSpeed),
+            Mathf.Lerp(transform.position.y, _player.transform.position.y, Time.deltaTime * _followSpeed)) + _offset;
     }
 }
